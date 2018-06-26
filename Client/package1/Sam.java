a4c6c2f57cfe2d3388b578e2cf04d4d0425853c6
Osama Abdullah <eng.samabdullah@gmail.com>
2018-06-26 14:07:04 -0700

    initial commit

---
+++ Client/package1/Sam.java
@@ -0,0 +1,14 @@
+package package1;
+
+class Sam {
+    Sam() {
+        super();
+    }
+
+    public static void main(String[] args) {
+        Sam sam = new Sam();
+    }
+    int x(){
+        return 2;
+    }
+}
---
+++ Client/package2/Sam2.java
@@ -0,0 +1,16 @@
+package package2;
+
+public class Sam2 {
+    public Sam2() {
+        super();
+    }
+
+    public static void main(String[] args) {
+        Sam2 sam2 = new Sam2();
+
+    }
+    public void m(){
+        
+ 
+    }
+}
